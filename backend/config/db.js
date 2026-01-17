4d6683c718081b7be213b4929662abda1d09477b
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-01-17 10:53:49 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://user123:user987@cluster0.exkxkun.mongodb.net/food-delivery').then(()=>{
+    await mongoose.connect('mongodb+srv://user123:user987@cluster0.ewco19f.mongodb.net/food-delivery').then(()=>{
        console.log('DB connected') ;
     })
 }
